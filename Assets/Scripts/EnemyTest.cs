bf6c034329c14c705de698ce8eab5bba1b3ebccc
cmilux <villa12marti@gmail.com>
2025-09-06 01:54:23 -0300

    Enemy Test - Player basic attack
    Added an enemy prefab to spawn rocks to it
    The enemy script was created
    Enemy tag created
    Joystick was tested (still needs to be worked on)

---
+++ Assets/Scripts/EnemyTest.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pickups"))
+        {
+            //If enemy collides with a rock ("Pickups"), destroy both
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
