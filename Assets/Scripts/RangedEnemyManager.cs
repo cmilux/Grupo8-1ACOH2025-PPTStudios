370d47060eefe6abb8f7dcc53fde16b52a0be084
selenabheim <155482573+bhselena@users.noreply.github.com>
2025-10-10 02:10:03 -0300

    Enemy patrol and follow state
    NEW:
    - Both type of enemies now have a patrol and follow state
    - Enemies now start the level patrolling, then switch to following the player once detected
    - Added missing annotations in both enemy managers scripts
    NEEDS FIXING
    - Enemies don't switch animation clips based on their axis of movement while in patrol state

--- Assets/Scripts/RangedEnemyManager.cs
+++ Assets/Scripts/RangedEnemyManager.cs
@@ -6,12 +6,19 @@ using UnityEngine.AI;
 using TMPro;
 using static UnityEngine.GraphicsBuffer;
 using UnityEngine.UI;
+using System.Linq;
 
 public class RangedEnemyManager : MonoBehaviour
 {
     [Header("Pathfinding AI")]
     [SerializeField] Transform _target;               // Gets target's transform
     [SerializeField] NavMeshAgent _agent;             // Gets the agent (enemy) NavMesh
+
+    [Header("Patrol logic")]
+    [SerializeField] public Vector3[] patrolPoints;   // Stores an index of positions to be reached by the enemy during patrol state
+    [SerializeField] int _currentPatrolPoint;         // Stores the current patrol point the enemy's moving towards
+    [SerializeField] float _enemySpeed;               // Sets the enemy's speed
+    [SerializeField] bool _playerDetected;            // Checks whether the enemy has detected the player or not
 
     [Header("Attack logic")]
     [SerializeField] bool _canAttack;                 // Checks whether enemy is able to attack or not
@@ -33,19 +40,28 @@ public class RangedEnemyManager : MonoBehaviour
     [SerializeField] GameObject inkSplatter;          // Gets the image for the ink splatter effect
     [SerializeField] PlayerHealth _playerHealth;      // Gets the player health script that calls for the ink splatter effect
     [SerializeField] TextMeshProUGUI _enemy2Health;   // Displays enemy health in UI
-    [SerializeField] Slider _healthBar;
+    [SerializeField] Slider _healthBar;               // Gets the enemy's health bar
 
     [Header("Animation")]
     [SerializeField] Animator _NPCAnimator;           // Reference to the NPC's Animator component
     [SerializeField] Animator _alienAnimator;         // Reference to the alien's Animator component
     [SerializeField] bool _playAttackAnimation;       // Checks if alien can play the attack animation
     [SerializeField] Vector2 _lastDir;                // Stores the last direction the enemy has moved in
-    [SerializeField] bool _enemyDamaged;
+    [SerializeField] bool _enemyDamaged;              // Checks whether the enemy has been damaged or not
 
     void Start()
     {
         // Gets the NavMeshAgent from the enemy
         _agent = GetComponent<NavMeshAgent>();
+
+        // Sets current attack cooldown timer to starting cooldown timer
+        _currentAttackCooldown = _attackCooldown;
+
+        // Sets current enemy health to the max health value
+        _currentEnemyHealth = _enemyHealth;
+
+        // Sets current patrol point to the first position on the index
+        _currentPatrolPoint = 0;
 
         //-------------------
         //2D NavMesh settings
@@ -56,17 +72,12 @@ public class RangedEnemyManager : MonoBehaviour
         // Should the agent movement ignore the vertical axis?
         _agent.updateUpAxis = false;
 
-        // Sets current attack cooldown timer to starting cooldown timer
-        _currentAttackCooldown = _attackCooldown;
-
-        _currentEnemyHealth = _enemyHealth;
+        // Disables the enemy's NavMeshAgent component until player is detected
+        _agent.enabled = false;
     }
 
     void Update()
     {
-        // Follows player
-        _agent.SetDestination(_target.position);
-
         // Checks distance with player to either stop or throw ink 
         CheckDistanceWithPlayer();
 
@@ -85,13 +96,26 @@ public class RangedEnemyManager : MonoBehaviour
         // Changes alien's sprites depending on their axes of movement (handled by StoreLastMove()) and if it's able to attack
         HandleAlienSprites();
 
+        // Updates health bar above enemy's head to the current health value 
         UpdateHealthBar();
-
+ 
         SettingUI();
 
+        // When the player is attacked by a ranged enemy, call for the ink splatter effect to be activated 
         if (_playerHealth.activateInkSplatterEffect == true)
         {
             StartCoroutine(InkSplatterEffect());
+        }
+
+        // Until player is detected, activate patrol state
+        if (!_playerDetected)
+        {
+            HandlePatrolState();
+        }
+        // When player is detected, activate follow state
+        else if (_playerDetected)
+        {
+            HandleFollowState();
         }
     }
 
@@ -116,8 +140,9 @@ public class RangedEnemyManager : MonoBehaviour
             // Checks enemy's health
             EnemyDeath();
         }
+        
         // Checks if enemy collides with the spray
-        else if (other.gameObject.CompareTag("Spray"))
+        if (other.gameObject.CompareTag("Spray"))
         {
             // Gets the damage value from the spray that the enemy has collided with
             int sprayDamage = other.gameObject.GetComponent<SprayManager>().sprayDamage;
@@ -131,6 +156,12 @@ public class RangedEnemyManager : MonoBehaviour
             EnemyDeath();
         }
         else _enemyDamaged = false;
+
+        // Checks if player enters the enemy's detection range
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _playerDetected = true;
+        }
     }
 
     private void EnemyDeath()
@@ -215,7 +246,7 @@ public class RangedEnemyManager : MonoBehaviour
         if (moveDir != Vector2.zero)
         {
             _lastDir = moveDir;
-        }
+        }  
     }
 
     private void HandleNPCSprites()
@@ -235,14 +266,65 @@ public class RangedEnemyManager : MonoBehaviour
     public IEnumerator InkSplatterEffect()
     {
         // Sets ink splatter effect active for a few seconds, then makes it inactive again
-        inkSplatter.SetActive(true);
-        yield return new WaitForSeconds(2.5f);
-        inkSplatter.SetActive(false);
-        _playerHealth.activateInkSplatterEffect = false;
+        if (inkSplatter != null)
+        {  
+            inkSplatter.SetActive(true);
+            yield return new WaitForSeconds(2.5f);
+            inkSplatter.SetActive(false);
+            _playerHealth.activateInkSplatterEffect = false;
+        }
     }
 
     private void UpdateHealthBar()
     {
         _healthBar.value = _currentEnemyHealth / _enemyHealth;
+    }
+
+    private void HandlePatrolState()
+    {
+        // Moves enemy towards the current patrol point
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[_currentPatrolPoint], _enemySpeed * Time.deltaTime);
+
+        // When the enemy has reached the current patrol point, call for an update to get the next position in the index
+        if (transform.position == patrolPoints[_currentPatrolPoint])
+        {
+            UpdatePatrolPoints();
+        }
+    }
+
+    private void UpdatePatrolPoints()
+    {
+        // Changes current patrol point to the next position in the index
+        _currentPatrolPoint++;
+
+        // When reaching the last position in the index, reset back to the first position
+        if (_currentPatrolPoint >= patrolPoints.Length)
+        {
+            _currentPatrolPoint = 0;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // When selecting the enemy, draw gizmos line between patrol points to show enemy patrol route in "Scene" mode
+        for (int i = 0; i < patrolPoints.Length; i++)
+        {
+            Vector3 p = patrolPoints[i];
+            Gizmos.DrawSphere(p, 0.12f);
+
+            if (i < patrolPoints.Length - 1)
+                Gizmos.DrawLine(p, patrolPoints[i + 1]);
+            else if (patrolPoints.Length > 1)
+                Gizmos.DrawLine(p, patrolPoints[0]);
+        }
+    }
+
+    private void HandleFollowState()
+    {
+        // Enables the enemy's NavMeshAgent component 
+        _agent.enabled = true;
+
+        // Sets the enemy's target to the player
+        _agent.SetDestination(_target.position);
     }
 }
