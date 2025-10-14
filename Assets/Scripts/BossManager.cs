fd9a6f48fa771f66e5ab3a1de86f4b96021d6b03
selenabheim <155482573+bhselena@users.noreply.github.com>
2025-10-14 00:14:06 -0300

    New "BossTest" scene + Boss ranged attack
    NEW:
    - Added new "BossTest" scene for testing the boss' placeholder mechanics
    - Added new "BossManager" script
    - Created boss prefab
    - Added boss placeholder sprite
    - Boss can now lose health from player's attacks
    - Boss now has a ranged attack with 3 ink bullets that spread depending on the player's position

---
+++ Assets/Scripts/BossManager.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] float _maxBossHealth;
+    [SerializeField] float _currentBossHealth;
+
+    [Header("Ranged Attack")]
+    [SerializeField] Transform _firingPoint;                // Stores the firing point where the boss's ink bullets will spawn from
+    [SerializeField] GameObject _inkPrefab;                 // Stores the ink bullet prefab
+    [SerializeField] GameObject _target;                    // Gets the player's game object
+    [SerializeField] float _spreadAngle;                    // Stores the angle at which the ink bullets will travel
+    [SerializeField] float _inkSpeed;                       // Stores the speed at which the ink bullets will travel
+    [SerializeField] float _rangedAttackCooldown;           // Stores the starting ranged attack cooldown timer at the start of the game
+    [SerializeField] float _currentRangedAttackCooldown;    // Stores the current ranged attack cooldown timer during the game
+
+    [Header("Melee Attack")]
+    [SerializeField] float _meleeAttackCooldown;            // Stores the starting melee attack cooldown timer at the start of the game
+    [SerializeField] float _currentMeleeAttackCooldown;     // Stores the current melee attack cooldown timer during the game
+
+
+    void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player");
+        
+        // Sets current attack cooldown timer to starting cooldown timer
+        _currentRangedAttackCooldown = _rangedAttackCooldown;
+
+        // Sets current boss health to the max health value
+        _currentBossHealth = _maxBossHealth;
+    }
+
+    private void Update()
+    {
+        // Checks both boss attacks cooldown timers and calls for whichever type of attack is available
+        HandleAttackCooldowns();
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        // Checks if boss collides with the spray
+        if (other.gameObject.CompareTag("Spray"))
+        {
+            // Gets the damage value from the spray that the boss has collided with
+            int sprayDamage = other.gameObject.GetComponent<SprayManager>().sprayDamage;
+
+            // Apply damage to boss
+            _currentBossHealth -= sprayDamage;
+
+            // Checks boss's health
+            BossDeath();
+        }
+
+        if (other.gameObject.CompareTag("Rock"))
+        {
+            // Gets the damage value from the rock that the boss has collided with
+            int rockDamageAmount = other.gameObject.GetComponent<RockManager>().rockDamage;
+
+            // Apply damage to boss
+            _currentBossHealth -= rockDamageAmount;
+
+            // Checks enemy's health
+            BossDeath();
+        }
+    }
+
+    void BossDeath()
+    {
+        // If boss' current health has reached 0...
+        if (_currentBossHealth <= 0)
+        {
+            // Destroy boss
+            Destroy(gameObject);
+        }
+    }
+
+    private void HandleAttackCooldowns()
+    {
+        // If ranged attack cooldown time is at zero...
+        if (_currentRangedAttackCooldown <= 0)
+        {
+            // Attack!
+            RangedAttack();
+
+            // And reset cooldown timer back to starting cooldown timer
+            _currentRangedAttackCooldown = _rangedAttackCooldown;
+        }
+        // If cooldown time is over zero, then tick down to zero
+        else
+        {
+            _currentRangedAttackCooldown -= Time.deltaTime;
+        }
+
+        // If melee attack cooldown time is at zero...
+        if (_currentMeleeAttackCooldown <= 0)
+        {
+            // Attack!
+            MeleeAttack();
+
+            // And reset cooldown timer back to starting cooldown timer
+            _currentMeleeAttackCooldown = _meleeAttackCooldown;
+        }
+        // If cooldown time is over zero, then tick down to zero
+        else
+        {
+            _currentMeleeAttackCooldown -= Time.deltaTime;
+        }
+    }
+
+    void RangedAttack()
+    {
+        // Calculates the player's direction
+        Vector2 targetPosition = (_target.transform.position - transform.position).normalized;
+
+        Vector2[] inkDirections = new Vector2[3];
+        inkDirections[0] = targetPosition;
+        inkDirections[1] = Quaternion.AngleAxis(_spreadAngle, Vector3.forward) * targetPosition;  
+        inkDirections[2] = Quaternion.AngleAxis(-_spreadAngle, Vector3.forward) * targetPosition;
+
+        foreach (Vector2 dir in inkDirections)
+        {
+            // Instancia la bala en el punto de disparo
+            var ink = Instantiate(_inkPrefab, _firingPoint.position, Quaternion.identity);
+
+            // Le da velocidad en la dirección correspondiente
+            ink.GetComponent<Rigidbody2D>().linearVelocity = dir * _inkSpeed;
+
+            // Destruye la bala luego de 2 segundos
+            Destroy(ink, 2f);
+        }
+    }
+
+    void MeleeAttack()
+    {
+
+    }
+}
